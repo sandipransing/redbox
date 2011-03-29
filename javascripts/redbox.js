785f07c985992e7eb7cae0281da63dccb1880e06
Sandip Ransing <sandip@joshsoftware.com>
2011-03-29 17:49:01 +0530

    Modified JS

--- javascripts/redbox.js
+++ javascripts/redbox.js
@@ -1,119 +1,50 @@
-
 var RedBox = {
+showInline: function(id){
+              this.showOverlay();
+              jQuery('#'+id).clone(true).css({display:'block'}).appendTo('#RB_window');
+              jQuery("#RB_window").show(0.3);
+              this.setWindowPosition();
+            },
 
-  showInline: function(id)
-  {
-    this.showOverlay();
-    new Effect.Appear('RB_window', {duration: 0.4, queue: 'end'});        
-    Element.scrollTo('RB_window');
-    this.cloneWindowContents(id);
-  },
+addHiddenContent: function(id){
+                    jQuery("#RB_window").children().remove();
+                    jQuery('#'+id).children().appendTo('#RB_window');
+                    jQuery("#RB_loading").hide();
+                    jQuery("#RB_window").show(0.3);
+                    this.setWindowPosition();
+                  },
 
-  loading: function()
-  {
-    this.showOverlay();
-    Element.show('RB_loading');
-    this.setWindowPosition();
-  },
+loading: function(){
+           this.showOverlay();
+           jQuery('#RB_loading').show();
+           this.setWindowPosition();
+         },
 
-  addHiddenContent: function(id)
-  {
-    this.removeChildrenFromNode($('RB_window'));
-    this.moveChildren($(id), $('RB_window'));
-    Element.hide('RB_loading');
-    new Effect.Appear('RB_window', {duration: 0.4, queue: 'end'});  
-    Element.scrollTo('RB_window');
-    this.setWindowPosition();
-  },
+close: function(){
+         jQuery("#RB_window").hide(0.3).children().remove();
+         jQuery("#RB_overlay").hide(0.3);
+       },
 
-  close: function()
-  {
-    new Effect.Fade('RB_window', {duration: 0.4});
-    new Effect.Fade('RB_overlay', {duration: 0.4});
-  },
+initContent: function(){
+               if (jQuery('#RB_redbox').length){
+                 jQuery('#RB_redbox').children().remove();
+                 jQuery('#RB_redbox').html('<div id="RB_window" style="display: none;"></div><div id="RB_overlay" style="display: none;"><div id="RB_loading" style="display: none"></div></div>');
+               }
+               else{
+                 jQuery('body').append('<div id="RB_redbox" align="center"><div id="RB_window" style="display: none;"></div><div id="RB_overlay" style="display: none;"><div id="RB_loading" style="display: none"></div></div></div>');
+               }
+             },
 
-  showOverlay: function()
-  {
-    if ($('RB_redbox'))
-    {
-      Element.update('RB_redbox', "");
-      new Insertion.Top($('RB_redbox'), '<div id="RB_window" style="display: none;"></div><div id="RB_overlay" style="display: none;"></div>');  
-    }
-    else
-    {
-      new Insertion.Bottom(document.body, '<div id="RB_redbox" align="center"><div id="RB_window" style="display: none;"></div><div id="RB_overlay" style="display: none;"></div></div>');      
-    }
-    new Insertion.Top('RB_overlay', '<div id="RB_loading" style="display: none"></div>');  
+showOverlay: function(){
+               this.initContent();
+               jQuery("#RB_overlay").css({height:jQuery(document).height(), opacity:0.6}).show(0.3);
+             },
 
-    this.setOverlaySize();
-    new Effect.Appear('RB_overlay', {duration: 0.4, to: 0.6, queue: 'end'});
-  },
-
-  setOverlaySize: function()
-  {
-    if (window.innerHeight && window.scrollMaxY)
-    {  
-      yScroll = window.innerHeight + window.scrollMaxY;
-    } 
-    else if (document.body.scrollHeight > document.body.offsetHeight)
-    { // all but Explorer Mac
-      yScroll = document.body.scrollHeight;
-    }
-    else
-    { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
-      yScroll = document.body.offsetHeight;
-    }
-    $("RB_overlay").style['height'] = yScroll +"px";
-  },
-
-  setWindowPosition: function()
-  {
-    var pagesize = this.getPageSize();  
-  
-    $("RB_window").style['width'] = 'auto';
-    $("RB_window").style['height'] = 'auto';
-
-    var dimensions = Element.getDimensions($("RB_window"));
-    var width = dimensions.width;
-    var height = dimensions.height;        
-    
-    $("RB_window").style['left'] = ((pagesize[0] - width)/2) + "px";
-    $("RB_window").style['top'] = ((pagesize[1] - height)/2) + "px";
-  },
-
-
-  getPageSize: function() {
-    var de = document.documentElement;
-    var w = window.innerWidth || self.innerWidth || (de&&de.clientWidth) || document.body.clientWidth;
-    var h = window.innerHeight || self.innerHeight || (de&&de.clientHeight) || document.body.clientHeight;
-  
-    arrayPageSize = new Array(w,h) 
-    return arrayPageSize;
-  },
-
-  removeChildrenFromNode: function(node)
-  {
-    while (node.hasChildNodes())
-    {
-      node.removeChild(node.firstChild);
-    }
-  },
-
-  moveChildren: function(source, destination)
-  {
-    while (source.hasChildNodes())
-    {
-      destination.appendChild(source.firstChild);
-    }
-  },
-
-  cloneWindowContents: function(id)
-  {
-    var content = $(id).cloneNode(true);
-    content.style['display'] = 'block';
-    $('RB_window').appendChild(content);  
-
-    this.setWindowPosition();
-  }
-
+setWindowPosition: function(){
+                     jQuery("#RB_window").css({width:'auto', height:'auto'});
+                     var left = (jQuery(window).width() - jQuery("#RB_window").width())/2;
+                     var top = (jQuery(window).height() - jQuery("#RB_window").height())/2 - 30;
+                     jQuery("#RB_window").css({left:left, top:top});
+                   }
 }
+
